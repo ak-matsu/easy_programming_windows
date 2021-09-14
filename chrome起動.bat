2f9ef5293e1170938186f27d6210382488f6ef5f
ak_matsu <42706619+ak-matsu@users.noreply.github.com>
2021-09-14 20:22:07 +0900

    Initial commit

---
+++ chrome起動.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --show-extension-s-on-top "https://site2.sbisec.co.jp/ETGate/" "https://trade.smbcnikko.co.jp/Login/0/login/ipan_web/hyoji/"
