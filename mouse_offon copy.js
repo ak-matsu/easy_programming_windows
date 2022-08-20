783534e3d09d55c0c453f9eb478b9e325120889a
ak-matsu <sandbox.matsu@gmail.com>
2022-08-21 02:19:13 +0900

    マウスのプロパティ起動ファイル作成

---
+++ mouse_offon copy.js
@@ -0,0 +1,13 @@
+//  Shell関連の操作を提供するオブジェクトを取得
+var sh = new ActiveXObject( "WScript.Shell" );
+
+//  マウスプロパティを起動
+sh.Run("control MAIN.CPL");
+
+//  起動してすぐにキー送信すると失敗する可能性があるので、3秒停止
+WScript.Sleep( 3000 );
+
+// WshShell.Run("control MAIN.CPL");
+// wait("ポインター オプション")
+// WshShell.SendKeys ("^"+"{TAB}");
+// WshShell.SendKeys ("^{TAB 3}");
